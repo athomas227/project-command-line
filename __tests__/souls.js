363bc4445fafea7590c5332ef8a8371c94a2d7c0
athomas227 <andrewthomas@pursuit.org>
2023-10-17 18:53:47 -0400

    added tests

---
+++ __tests__/souls.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const souls = require("../data/souls")
+const {
+    addSoul,
+    getAllSouls,
+    getSoulByID,
+    updateSoul,
+    removeSoul,
+    generateSouls,
+    getRandomNum
+  } = require("../src/souls");
+
+
+  test('getAllSouls should return the souls array', () => {
+    const allSouls = getAllSouls();
+    expect(allSouls).toEqual(souls);
+  });
+
+  test('getSoulByID should return the soul with the specified ID', () => {
+    const id = "d-";
+    const soul = getSoulByID(id);
+    expect(soul.id).toBe(id);
+  });
