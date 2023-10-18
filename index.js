2c7e130f722f09781a90d92545d1cc5a9e483157
athomas227 <andrewthomas@pursuit.org>
2023-10-17 21:49:30 -0400

    added comments on index.js file

--- index.js
+++ index.js
@@ -14,20 +14,27 @@ const {
     cancelCart
 } = require('./src/souls');
 
+// Define a red color for error messages.
 const red = chalk.red.bold
+
+// Get the command from the command-line arguments.
 const command = process.argv[2];
 
+// Switch statement to handle different commands.
 switch (command) {
+    // 'add' command: Add a new soul.
     case 'add':
         const newSoul = addSoul();
         console.log('Added new soul:', newSoul);
         break;
 
+    // 'list' command: List all available souls.    
     case 'list':
         const allSouls = getAllSouls();
         console.log('All souls:', allSouls);
         break;
 
+    // 'details' command: Get details of a specific soul.    
     case 'details':
         const soulId = process.argv[3];
         if (!soulId) {
@@ -42,6 +49,7 @@ switch (command) {
         }
         break;
 
+    // 'delete' command: Delete a specific soul.    
     case 'delete':
         const soulIdToDelete = process.argv[3];
         if (!soulIdToDelete) {
@@ -52,6 +60,7 @@ switch (command) {
         }
         break;
 
+    // 'update' command: Update the details of a specific soul.    
     case 'update':
         const soulIdToUpdate = process.argv[3];
         const updatedSoul = JSON.parse(process.argv[4]);
@@ -67,6 +76,7 @@ switch (command) {
         }
         break;
 
+    // 'addToCart' command: Add a soul to the shopping cart.    
     case 'addToCart':
         const soulIdToAddToCart = process.argv[3];
         const quantityToAddToCart = parseInt(process.argv[4] || 1);
@@ -78,17 +88,20 @@ switch (command) {
         }
         break;
 
+    // 'viewCart' command: View the contents of the shopping cart.   
     case 'viewCart':
         const cartContents = viewCart();
         console.log('Cart:', cartContents);
         console.log('Total Price:', calculateCartTotal());
         break;
 
+    // 'cancelCart' command: Empty the shopping cart.    
     case 'cancelCart':
         cancelCart();
         console.log(red('Cart emptied.'));
         break;
 
+    // Default case: Handle invalid commands.    
     default:
         console.log(red('Invalid command. Please use one of the following commands: add, list, details, delete, update, addToCart, viewCart, cancelCart'));
         break;
